7fda3bb36ac23bfbcc5f3d186b431c60a798ee4f
KulkarniSwapnil10 <swapnil.kulkarni10@outlook.com>
2025-11-06 00:02:30 +0530

    Added All the files of Working Project
    Working project of NXP5748G_CAN_Tx_Rx

---
+++ NXP5748G_CAN_Tx_Rx_Example/can_pal_mpc5748g/Debug_FLASH/Sources/main.d
@@ -0,0 +1,148 @@
+Sources/main.o: ../Sources/main.c \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/lib_c99.prefix \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/common.prefix \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_c_version.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/Cpu.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/device_registers.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/common/s32_core_e200.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/MPC5748G/include/MPC5748G.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stdint.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ansi_parms.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewlGlobals.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/pa/ansi_prefix.PA_EABI.bare.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/os_enum.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/cstdint \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/limits_api.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_lib_ext1.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/wchar_t.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/MPC5748G/include/MPC5748G_features.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/devassert.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stdbool.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/clock.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/status.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/../src/clock/MPC57xx/clock_MPC57xx.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/edma_driver.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stddef.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/cstddef \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/null.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/size_t.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/max_align_t.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_rsize_t.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/rtos/osif/osif.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/pal/inc/can_pal.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/can_pal_cfg.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/pal/inc/can_pal_mapping.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/flexcan_driver.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/edma_driver.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/callbacks.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/MPC5748G/startup/system_MPC5748G.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/clockMan1.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/Cpu.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/dmaController1.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/pin_mux.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/pins_driver.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/../src/pins/siul2/pins_siul2_driver.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/CAN_1.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/clockMan1.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/CAN_1.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/dmaController1.h \
+ C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/pin_mux.h
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/lib_c99.prefix:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/common.prefix:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_c_version.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/Cpu.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/device_registers.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/common/s32_core_e200.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/MPC5748G/include/MPC5748G.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stdint.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ansi_parms.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewlGlobals.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/pa/ansi_prefix.PA_EABI.bare.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/os_enum.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/cstdint:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/limits_api.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_lib_ext1.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/wchar_t.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/MPC5748G/include/MPC5748G_features.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/devassert.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stdbool.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/clock.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/status.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/../src/clock/MPC57xx/clock_MPC57xx.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/edma_driver.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stddef.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/cstddef:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/null.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/size_t.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/max_align_t.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_rsize_t.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/rtos/osif/osif.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/pal/inc/can_pal.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/can_pal_cfg.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/pal/inc/can_pal_mapping.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/flexcan_driver.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/edma_driver.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/callbacks.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/devices/MPC5748G/startup/system_MPC5748G.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/clockMan1.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/Cpu.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/dmaController1.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/pin_mux.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/pins_driver.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/S32_SDK_S32PA_RTM_3.0.0/platform/drivers/inc/../src/pins/siul2/pins_siul2_driver.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/CAN_1.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/clockMan1.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/CAN_1.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/dmaController1.h:
+
+C:/Users/lenovo/workspaceS32DS.Power.2.1/can_pal_mpc5748g/Generated_Code/pin_mux.h:
